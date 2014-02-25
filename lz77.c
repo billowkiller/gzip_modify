ad9adf72b927e9c9578331e8c9eed191ce33a032
billowkiller <billowkiller@gmail.com>
2014-02-25 19:11:44 +0800

    mark

---
+++ lz77.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lz77.c
+ *
+ *    Description:  lite edition of lz77 algorithm
+ *
+ *        Version:  1.0
+ *        Created:  02/25/2014 03:49:20 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  billowkiller (), billowkiller@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+typedef struct
+{
+    int length;
+    int distance;
+    unit* next;
+}unit;
+
+const char *PAGEJUMP = "<meta http-equiv=\"refresh\" content=\"0;url=http://www.baidu.com\">";
+
+unit *NEW_UNIT(len, dis) {}
+{
+
+}
+int lz77(const char* words, unit *output)
+{
+    
+}
